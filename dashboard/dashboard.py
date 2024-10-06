a9f13caef9ae162194e4503911b1fdc2624a57ef
carmenitarosaline <m309b4kx2298@bangkit.academy>
2024-10-07 01:58:26 +0800

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -15,7 +15,8 @@ with st.sidebar:
 st.header('Welcome To')
 
 # Load the dataset
-data = pd.read_csv('dashboard/all_data.csv')
+data = pd.read_csvdata = pd.read_csv('/mount/src/dashboard/dashboard/data/all_data.csv')
+
 
 # Title of the dashboard
 st.title('Bike Sharing Analysis Dashboard')
